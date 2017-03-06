9d45a2a20714e7b4c5658b05965a70e5e0a1e24d
Costa Tsaousis <costa@tsaousis.gr>
2017-03-06 16:28:42 +0000

    Mon Mar  6 16:28:41 UTC 2017 update

--- set_file_timestamps.sh
+++ set_file_timestamps.sh
@@ -41,7 +41,7 @@
 [ -f 'bambenek_ramnit.ipset' ] && /usr/bin/touch --date=@1488780393 'bambenek_ramnit.ipset'
 [ -f 'bambenek_ranbyus.ipset' ] && /usr/bin/touch --date=@1488809838 'bambenek_ranbyus.ipset'
 [ -f 'bambenek_simda.ipset' ] && /usr/bin/touch --date=@1488813365 'bambenek_simda.ipset'
-[ -f 'bambenek_suppobox.ipset' ] && /usr/bin/touch --date=@1488813771 'bambenek_suppobox.ipset'
+[ -f 'bambenek_suppobox.ipset' ] && /usr/bin/touch --date=@1488817479 'bambenek_suppobox.ipset'
 [ -f 'bambenek_symmi.ipset' ] && /usr/bin/touch --date=@1479352111 'bambenek_symmi.ipset'
 [ -f 'bambenek_tinba.ipset' ] && /usr/bin/touch --date=@1488816215 'bambenek_tinba.ipset'
 [ -f 'bambenek_volatile.ipset' ] && /usr/bin/touch --date=@1487956121 'bambenek_volatile.ipset'
